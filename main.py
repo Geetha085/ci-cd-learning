ca305255abf19e0d58ec4b307d006ba1ebee0aa6
Geetha <geethashree204@gmail.com>
2026-02-05 10:46:50 +0530

    Update success message

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Deployment Successful!"
+    return "My CI/CD is Fully Working!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
